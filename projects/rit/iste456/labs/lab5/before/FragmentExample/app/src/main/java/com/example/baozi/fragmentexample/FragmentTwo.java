5b76010589e175415ec75ec434a4ee9e43d1a77d
baozi <yqg900045@gmail.com>
2016-03-05 04:02:09 -0500

    Rebuild pledge allegiance app

---
+++ projects/rit/iste456/labs/lab5/before/FragmentExample/app/src/main/java/com/example/baozi/fragmentexample/FragmentTwo.java
@@ -0,0 +1,19 @@
+package com.example.baozi.fragmentexample;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by baozi on 2/25/16.
+ */
+public class FragmentTwo extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState){
+        //inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_two,container,false);
+    }
+}
